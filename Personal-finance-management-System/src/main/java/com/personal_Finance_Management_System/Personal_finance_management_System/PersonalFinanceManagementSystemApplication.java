557046afb549bf9736723c7febd4095c12c650ec
Bhaveshpatil45 <bp700259@gmail.com>
2025-03-14 11:43:08 +0530

    Personal-finance-SpringBoot

---
+++ Personal-finance-management-System/src/main/java/com/personal_Finance_Management_System/Personal_finance_management_System/PersonalFinanceManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.personal_Finance_Management_System.Personal_finance_management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceManagementSystemApplication.class, args);
+	}
+
+}
