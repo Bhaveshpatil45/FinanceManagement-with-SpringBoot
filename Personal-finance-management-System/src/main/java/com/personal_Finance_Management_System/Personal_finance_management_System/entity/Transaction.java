d75cbb640e1839d945478f2918fd4760c8d2da27
Bhaveshpatil45 <bp700259@gmail.com>
2025-04-11 19:24:39 +0530

    Personal-Finance

---
+++ Personal-finance-management-System/src/main/java/com/personal_Finance_Management_System/Personal_finance_management_System/entity/Transaction.java
@@ -0,0 +1,88 @@
+package com.personal_Finance_Management_System.Personal_finance_management_System.entity;
+
+import java.time.LocalDate;
+
+public class Transaction {
+
+    private long id;
+
+    private double amount;
+
+    private String type;
+
+    private String category;
+
+    private LocalDate date;
+
+    private String description;
+
+    private User user;
+
+    public Transaction() {}
+
+    public Transaction(long id, double amount, String type, String category, LocalDate date, String description, User user) {
+        this.id = id;
+        this.amount = amount;
+        this.type = type;
+        this.category = category;
+        this.date = date;
+        this.description = description;
+        this.user = user;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
